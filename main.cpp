6ac295c18fa55ef55c69bb7c798ed3707488594c
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-01-27 08:42:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Imports
 #include <iostream>
 #include <cstring>
 #include <iomanip>
@@ -6,12 +7,14 @@
 
 using namespace std;
 
+// Prototypes
 void addStudent(Node* &headNode, Student* newStudent);
 void findAvg(Node* headNode, int counter, double avg); 
 void printStudent(Node* headNode);
 void deleteStudent(Node* &headNode, int myID); 
 
 int main() {
+  // Variable initialization
   Node* head = NULL;
   char input[20];
   int myID;
@@ -20,11 +23,15 @@ int main() {
   double avg = 0;
   bool active = true;
   cout << "Welcome to Student List: Linked List Edition." << endl; 
+  
+  // While loop to keep track of game running
   while(active == true) {
     cout << "Your options are \"ADD,\" \"DELETE,\" \"PRINT,\" \"AVERAGE,\" and \"QUIT.\"" << endl;
     cin >> input;
     cin.clear();
     cin.ignore(10000, '\n');
+    
+    // If the user wants to add a student, names, ID, and GPA are collected and stored in a new Student. addStudent is then called.
     if(strcmp(input, "ADD") == 0){
       Student* newStudent = new Student();
       cout << "Enter the student's first name:" << endl; 
@@ -38,6 +45,8 @@ int main() {
       cin >> myGPA;
       newStudent->setGPA(myGPA);
       addStudent(head, newStudent);
+      
+      // If the head node is null, the deleteStudent function won't run. Otherwise, it asks for the ID and then calls the function.
     } else if(strcmp(input, "DELETE") == 0){
       if(head == NULL) {
         cout << "No students in list." << endl; 
@@ -46,12 +55,16 @@ int main() {
         cin >> myID;
         deleteStudent(head, myID);
       }      
+      
+      // calls printStudent
     } else if(strcmp(input, "PRINT") == 0){
       if(head == NULL) {
         cout << "No students in list." << endl; 
       } else { 
         printStudent(head);
       }
+      
+      // calls findAvg
     } else if(strcmp(input, "AVERAGE") == 0){
       if(head == NULL) {
         cout << "No students in list." << endl; 
@@ -69,23 +82,29 @@ int main() {
 }
 
 void addStudent(Node* &headNode, Student* newStudent) {
+  // If the head node is null, a new head node is created using the new Student.
   if(headNode == NULL) {
     headNode = new Node(newStudent);
     cout << "Student added." << endl;
+    // If the new Student's GPA is higher than the head Node, a new head node is created that links to the previous head node.
   } else if(headNode->getStudent()->getID() > newStudent->getID()) {
     Node* tempNode = headNode;
     headNode = new Node(newStudent);
     headNode->setNext(tempNode);
     cout << "Student added." << endl;
+    // If the next node is null, a new node is created and the head node links to that new node
   } else if(headNode->getNext() == NULL) {
     Node* tempNode4 = new Node(newStudent);
     headNode->setNext(tempNode4);
     cout << "Student added." << endl;
+    // If the student's GPA is between two nodes, a new node is created with that new Student. This node then links to the head node's next next node. 
+    // Finally, the head node links to the new node. 
   } else if(newStudent->getID() <= headNode->getNext()->getStudent()->getID() && newStudent->getID() >= headNode->getStudent()->getID()) {
     Node* tempNode2 = new Node(newStudent);
     tempNode2->setNext(headNode->getNext());
     headNode->setNext(tempNode2);
     cout << "Student added." << endl;
+    // If no conditions are met, the function is called again (recursive).
   } else {
     Node* tempNode3 = headNode->getNext();
     addStudent(tempNode3, newStudent);  
